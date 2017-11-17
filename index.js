96325325189ac53af87514e5b6fdf3683e12db43
Bryan Ortiz <bryan@bryanortiz.com>
2017-11-17 02:59:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ var height = "74"
 
 if (name === "Susan") {
     console.log("The name is correct")
+}else{
+  console.log('Nope!')
 }
 
 // Don't worry about this
 module.exports = { name, height
 }
-
